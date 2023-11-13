1ddf3f546dd9d4753bca0d5fab64d8a407ce75e0
javierac11 <javieracedocastrillon@gmail.com>
2023-11-13 09:48:07 +0100

    Ejercicio 4 comentado

--- src/Ejercicio4.py
+++ src/Ejercicio4.py
@@ -9,15 +9,41 @@ frutas2 = ["manzana", "pera", "durazno", "sandía", "uva"]
 #    Encuentra las frutas que están en frutas1 pero no en frutas2 y guárdalas en un conjunto llamado frutas_solo_en_frutas1.
 #    Encuentra las frutas que están en frutas2 pero no en frutas1 y guárdalas en un conjunto llamado frutas_solo_en_frutas2.
 
-def crearConjuntoConLista(lista: list):
+def crearConjuntoConLista(lista: list) -> set:
+    """Convierte una lista en un conjunto
+
+    Args:
+        lista (list): Una lista
+
+    Returns:
+        set: la lista convertida en un conjunto
+    """
     conjunto = set(lista)
     return conjunto
 
-def encontrarRepetidos(conjunto1: set, conjunto2: set):
+def encontrarRepetidos(conjunto1: set, conjunto2: set) -> set:
+    """Encuentra los repetidos de 2 conjuntos
+
+    Args:
+        conjunto1 (set): Un conjunto
+        conjunto2 (set): Otro conjunto
+
+    Returns:
+        set: Los repetidos en los 2 conjuntos
+    """
     comunes = conjunto1 & conjunto2
     return comunes
 
 def elementosConjunto1NoEstenConjunto2(conjunto1: set, conjunto2: set):
+    """Busca los elementos del conjunto1 que no esten en el conjunto2
+
+    Args:
+        conjunto1 (set): Un conjunto
+        conjunto2 (set): Otro conjunto
+
+    Returns:
+        set: Elementos del conjunto1 que no esten en el conjunto2
+    """
     no_comunes = conjunto1 - conjunto2
     return no_comunes
 
